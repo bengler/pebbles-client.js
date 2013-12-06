3e3255b2d3a7afbee7c91bea8ea427966aa09bb6
Bjørge Næss <bjoerge@bengler.no>
2013-12-06 12:16:43 +0100

    Complete xhr adapter

--- adapters/xhr.js
+++ adapters/xhr.js
@@ -1,12 +1,27 @@
 "use strict";
 
 var xhr = require("xhr")
+var url = require("url")
+var extend = require("util-extend");
 
-module.exports = function request(method, url, params, opts, callback) {
-  return xhr({
-    cors: true,
-    method: method,
-    uri: url,
-    json: params
-  }, callback);
+var defaultOpts = {
+  cors: true
+};
+
+module.exports = function request(options, callback) {
+  var requestOpts = extend({}, defaultOpts);
+  requestOpts.method = options.method;
+  requestOpts.uri = options.url;
+  
+  if (options.queryString) {
+    var u = url.parse(requestOpts.uri, true, true)
+    u.query = extend(u.query, options.queryString)
+    requestOpts.uri = url.format(u);
+  }
+  if (requestOpts.body) {
+    requestOpts.json = options.body;
+  }
+
+  callback || (callback = function() {})
+  return xhr(requestOpts, callback);
 };
