4c028573e799a8ba737bdb9d248252df65b228f5
Bjørge Næss <bjoerge@gmail.com>
2014-10-13 20:48:01 +0200

    Remove require() for removed client

--- clients/index.js
+++ clients/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   checkpoint: require("./checkpoint"),
   tiramisu: require("./tiramisu"),
-  tiramisuIframe: require("./tiramisu/iframe"),
   reaktorCore: require("./reaktorcore")
 };
