316daf9442310fb87adccc591a2e101a77045232
Bjørge Næss <bjoerge@gmail.com>
2014-10-13 20:43:32 +0200

    Simplify tiramisu client

--- clients/tiramisu/index.js
+++ clients/tiramisu/index.js
@@ -2,10 +2,23 @@
 
 var Client = require("../../client");
 var inherits = require("inherits");
-var ProgressStream = require("./xhr-progress-stream")
 var JSONStream = require('json-stream');
+var through = require('through');
 
-module.exports = TiramisuClient
+module.exports = TiramisuClient;
+
+var normalizers = {
+  uploading: function(p)    { return p * 0.6      },
+  received: function(p)     { return 60           },
+  transferring: function(p) { return 60 + p * 0.4 },
+  completed: function(p)    { return 100          }
+};
+
+function normalizeProgress(event) {
+  this.queue(Object.assign({}, event, {
+    percent: normalizers[event.status](event.percent)
+  }))
+}
 
 function TiramisuClient() {
   Client.apply(this, arguments);
@@ -13,24 +26,22 @@ function TiramisuClient() {
 
 inherits(TiramisuClient, Client);
 
-TiramisuClient.prototype.upload = function (endpoint, fileField, cb) {
+TiramisuClient.prototype.upload = function (endpoint, file) {
 
-  var formData = new FormData();
-  formData.append(fileField.name, fileField.files[0]);
+  var formData = new window.FormData();
+  formData.append('file', file);
 
-  var xhr = new XMLHttpRequest();
-  var progress = new ProgressStream(xhr);
-  xhr.open("POST", this.urlTo(endpoint));
-  xhr.withCredentials = true;
-  var error = null;
-  progress.on('error', function (e) {
-    error = e;
+  var req = this.stream().post(endpoint);
+
+  req.xhr.upload.addEventListener("progress", function (progressEvent) {
+    var percent = progressEvent.lengthComputable ? Math.ceil((progressEvent.loaded / progressEvent.total) * 100) : -1;
+    req.push('{"percent": ' + percent + ',"status": "uploading"}\n');
   });
-  if (typeof cb == 'function') {
-    progress.on('end', function (body) {
-      cb(error, body, progress);
-    });
-  }
-  xhr.send(formData);
-  return progress.pipe(new JSONStream())
+
+  req.end(formData);
+
+  return req.pipe(new JSONStream()).pipe(through(normalizeProgress)).pipe(through(function(data) {
+    console.log(data);
+    this.queue(data);
+  }));
 };
