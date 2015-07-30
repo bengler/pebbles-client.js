1acec2c46264ec26e6f7b4e26ee8d704ae954342
Bjørge Næss <bjoerge@gmail.com>
2015-07-30 15:33:33 +0200

    Make progressive version polling in tiramisu client more resilient for missing matches

--- clients/tiramisu/index.js
+++ clients/tiramisu/index.js
@@ -1,31 +1,25 @@
 "use strict";
 
-var Client = require("../../client");
-var inherits = require("inherits");
+var Client = require('../../client');
+var inherits = require('inherits');
 var JSONStream = require('json-stream');
 var through = require('through');
 
 module.exports = TiramisuClient;
 
 var normalizers = {
-  uploading: function(p) { return p * 0.6 },
-  received: function(p) { return 60 },
-  transferring: function(p) { return 60 + p * 0.2 },
-  transferred: function(p) { return 80 },
-  ready: function(p) { return 80 + p * 0.2 },
-  completed: function(p) { return 100 }
+  initializing: function (p) { return 0; },
+  uploading: function (p) { return p * 0.6; },
+  received: function (p) { return 60; },
+  transferring: function (p) { return 60 + p * 0.2; },
+  transferred: function (p) { return 80; },
+  ready: function (p) { return 80 + p * 0.2; },
+  completed: function (p) { return 100; }
 };
 
-function normalizeProgress() {
-  return through(function write(event) {
-    this.queue(Object.assign({}, event, {
-      percent: normalizers[event.status](event.percent)
-    }));
-  })
-}
-
-function TiramisuClient() {
-  Client.apply(this, arguments);
+function TiramisuClient(options) {
+  options.service = 'tiramisu';
+  Client.call(this, options);
 }
 
 inherits(TiramisuClient, Client);
@@ -33,12 +27,12 @@ inherits(TiramisuClient, Client);
 TiramisuClient.prototype.uploadImage = function (endpoint, file, options) {
   return this.uploadFile(endpoint, file)
     .pipe(this.waitFor(options.waitFor))
-    .pipe(normalizeProgress())
+    .pipe(this._normalizeProgress());
 };
 
 TiramisuClient.prototype.uploadFile = function (endpoint, file) {
   return this.upload(endpoint, file)
-    .pipe(normalizeProgress());
+    .pipe(this._normalizeProgress());
 };
 
 TiramisuClient.prototype.upload = function (endpoint, file) {
@@ -48,7 +42,7 @@ TiramisuClient.prototype.upload = function (endpoint, file) {
 
   var req = this.stream().post(endpoint);
 
-  req.xhr.upload.addEventListener("progress", function (progressEvent) {
+  req.xhr.upload.addEventListener('progress', function (progressEvent) {
     var percent = progressEvent.lengthComputable ? Math.ceil((progressEvent.loaded / progressEvent.total) * 100) : -1;
     req.push('{"percent": ' + percent + ',"status": "uploading"}\n');
   });
@@ -56,6 +50,14 @@ TiramisuClient.prototype.upload = function (endpoint, file) {
   req.end(formData);
 
   return req.pipe(new JSONStream());
+};
+
+TiramisuClient.prototype._normalizeProgress = function _normalizeProgress() {
+  return through(function write(event) {
+    this.queue(Object.assign({}, event, {
+      percent: normalizers[event.status](event.percent)
+    }));
+  });
 };
 
 TiramisuClient.prototype.waitFor = function waitFor(versionMatchFn) {
@@ -75,23 +77,26 @@ TiramisuClient.prototype.waitFor = function waitFor(versionMatchFn) {
 
   function end() {
     // Rename the 'completed' event to 'transferred' as it is not completed just yet :)
-    stream.queue(Object.assign(completedEvent, { status: 'transferred' }));
+    stream.queue(Object.assign({}, completedEvent, {status: 'transferred'}));
 
     pendingVersions = completedEvent.metadata.versions;
     if (versionMatchFn) {
-      pendingVersions = pendingVersions.slice(0, pendingVersions.findIndex(versionMatchFn) + 1);
+      var matchingVersionIndex = pendingVersions.findIndex(versionMatchFn);
+      if (matchingVersionIndex > -1) {
+        pendingVersions = pendingVersions.slice(0, matchingVersionIndex + 1);
+      }
     }
     waitForCount = pendingVersions.length;
 
-    poll().then(function(readyVersion) {
+    pollNext().then(function (readyVersion) {
       stream.queue({status: 'completed', ready: readyVersion, metadata: completedEvent.metadata, percent: 100});
       stream.queue(null);
     });
   }
 
-  function poll() {
+  function pollNext() {
     return waitForVersion(pendingVersions.shift())
-      .then(function(readyVersion) {
+      .then(function (readyVersion) {
         var percent = 100 / waitForCount * (waitForCount - pendingVersions.length);
 
         stream.queue({status: 'ready', version: readyVersion, metadata: completedEvent.metadata, percent: percent});
@@ -99,14 +104,15 @@ TiramisuClient.prototype.waitFor = function waitFor(versionMatchFn) {
         if (pendingVersions.length == 0) {
           return readyVersion;
         }
-        return poll();
-      })
+        return pollNext();
+      });
   }
+
   return stream;
 };
 
-function waitForVersion(version, opts) {
-  opts = opts || {};
+function waitForVersion(version, _opts) {
+  var opts = _opts || {};
   opts.timeout = opts.timeout || 1000 * 60 * 5;
   opts.pollInterval = opts.pollInterval || 1000;
 
@@ -120,16 +126,21 @@ function waitForVersion(version, opts) {
       });
   }
 
-  return timeout(poll(), opts.timeout, "Transcoding timed out after " + opts.timeout + "ms");
+  return timeout(poll(), opts.timeout, 'Transcoding timed out after ' + opts.timeout + 'ms');
 }
 
 // A few util functions
 function _checkS3(url) {
   var req = new XMLHttpRequest();
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     req.open('HEAD', url, true);
-    req.onload = function() {
-      if (req.status == 403) { reject() } else { resolve() }
+    req.onload = function () {
+      if (req.status == 403) {
+        reject();
+      }
+      else {
+        resolve();
+      }
     };
     req.onerror = reject;
     req.send();
@@ -137,13 +148,13 @@ function _checkS3(url) {
 }
 
 function delay(ms) {
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     setTimeout(resolve, ms);
   });
 }
 
-function timeout(promise, time) {
+function timeout(promise, time, error) {
   return Promise.race([promise, delay(time).then(function () {
-    throw new Error('Operation timed out');
+    throw new Error(error || 'Operation timed out');
   })]);
 }
---
+++ test/clients/tiramisu.js
@@ -0,0 +1,138 @@
+'use strict'
+
+var assert = require('assert')
+var TiramisuClient = require('../../clients/tiramisu')
+var Connector = require('../../connector');
+var through = require('through');
+var concat = require('concat-stream');
+
+function step(status, percent, metadata) {
+  var ret = {status: status, percent: percent};
+  if (metadata) {
+    ret.metadata = metadata
+  }
+  return ret;
+}
+
+function createProgress() {
+  var url = 'http://staging.o5.no.s3.amazonaws.com/oma/www/20150729093106-ucoz-1315-gif/234.gif';
+  var aspect = 1.3146;
+
+  return [
+    ['initializing', 1, {type: 'image', previewUrl: url}],
+    ['uploading', 10],
+    ['uploading', 20],
+    ['uploading', 30],
+    ['uploading', 40],
+    ['received', 40],
+    ['transferring', 50],
+    ['transferring', 60],
+    ['transferring', 70],
+    ['transferring', 80],
+    ['transferred', 85],
+    ['completed', 100, {
+      type: 'image',
+      original: 'http://staging.o5.no.s3.amazonaws.com/oma/www/20150729093106-ucoz-1315-gif/original.gif',
+      fullsize: 'http://staging.o5.no.s3.amazonaws.com/oma/www/20150729093106-ucoz-1315-gif/1213.gif',
+      aspectRatio: aspect,
+      tiramisuId: 'image:oma.www$20150520212659-8fqm-1468-jpg',
+      versions: [
+        {
+          width: 100,
+          square: false,
+          url: url + '?width=100'
+        },
+        {
+          width: 100,
+          square: true,
+          url: url + '?width=100&square=true'
+        },
+        {
+          width: 300,
+          square: false,
+          url: url + '?width=300'
+        },
+        {
+          width: 1213,
+          square: false,
+          url: url + '?width=1213'
+        }
+      ]
+    }]
+  ]
+}
+var PROGRESS = createProgress();
+
+var noop = function () {
+};
+
+// Create a mock XMLHttpRequest
+global.XMLHttpRequest = function () {
+  var req = {};
+  req.open = function (method, url) {
+    //console.log("Request: %s %s", method, url)
+  };
+  req.send = function () {
+    req.status = 200;
+    setTimeout(req.onload, 0);
+  };
+  return req;
+};
+
+
+describe('TiramisuClient', function () {
+
+  var connector = new Connector({adapter: {stream: noop}});
+
+  var client = new TiramisuClient({connector: connector});
+
+  describe('#waitFor', function () {
+    xit('progressively polls for versions up to the size of the version specified by the given matchFn', function (done) {
+
+    });
+
+    it('Waits for a version match specified by the given matchFn', function (done) {
+      var uploadProgress = through();
+
+      uploadProgress
+        .pipe(client.waitFor(function (version) {
+          return version.width >= 100;
+        }))
+        .pipe(client._normalizeProgress())
+        .pipe(concat(function (result) {
+          var lastReadyVersion = result.slice(-2)[0];
+          assert.equal(lastReadyVersion.status, 'ready', 'Unexpected status of second last progress event');
+          assert.equal(lastReadyVersion.version.width, 100, 'Unexpected width of last ready image');
+          done();
+        }));
+
+      PROGRESS.forEach(function (p) {
+        uploadProgress.push(step.apply(null, p))
+      })
+
+      uploadProgress.end();
+    })
+
+    it('Falls back to the largest possible image if there are no version match', function (done) {
+      var uploadProgress = through();
+
+      uploadProgress
+        .pipe(client.waitFor(function (version) {
+          return version.width > 1300;
+        }))
+        .pipe(client._normalizeProgress())
+        .pipe(concat(function (result) {
+          var lastReadyVersion = result.slice(-2)[0];
+          assert.equal(lastReadyVersion.status, 'ready', 'Unexpected status of second last progress event');
+          assert.equal(lastReadyVersion.version.width, 1213, 'Unexpected width of last ready image');
+          done();
+        }));
+
+      PROGRESS.forEach(function (p) {
+        uploadProgress.push(step.apply(null, p));
+      })
+
+      uploadProgress.end();
+    })
+  })
+});
